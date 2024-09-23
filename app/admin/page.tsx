3c64e199f220a40921835a54ad1f10495d059b40
nghiakma <lam47788@gmail.com>
2024-09-23 18:28:47 +0700

    sidebar

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,21 +1,27 @@
 "use client";
 import React from "react";
 import Heading from "../utils/Heading";
+import AdminSidebar from "../components/Admin/sidebar/AdminSidebar";
 import AdminProtected from "../hook/adminProtected";
+
+
 type Props = {};
 
 const page = (props: Props) => {
   return (
     <div>
-     <AdminProtected>
-     <Heading
+      <AdminProtected>
+        <Heading
           title="Admin"
           description="Hỗ trợ học tập"
           keywords="Nextjs, tailwind"
         />
-
-        
-     </AdminProtected>
+        <div className="flex min-h-screen">
+          <div className="1500px:w-[16%] w-1/5">
+            <AdminSidebar />
+          </div>
+        </div>
+      </AdminProtected>
     </div>
   );
 };
--- app/components/Admin/sidebar/AdminSidebar.tsx
+++ app/components/Admin/sidebar/AdminSidebar.tsx
@@ -28,78 +28,78 @@ import { useTheme } from "next-themes";
 import Cookies from "js-cookie";
 
 interface itemProps {
-    title: string;
-    to: string;
-    icon: JSX.Element;
-    selected: string;
-    setSelected: any;
+  title: string;
+  to: string;
+  icon: JSX.Element;
+  selected: string;
+  setSelected: any;
 }
 
 const Item: FC<itemProps> = ({ title, to, icon, selected, setSelected }) => {
-    return (
-      <MenuItem
-        active={selected === title}
-        onClick={() => setSelected(title)}
-        icon={icon}
-        className="hover:!bg-[unset]"//ghi đè lên bất kỳ màu nền nào khác 
-      >
-        <Link href={to} className="hover:!bg-[unset]">
-          <Typography className="!text-[16px] !font-Poppins text-black dark:text-white">
-            {title}
-          </Typography>
-        </Link>
-      </MenuItem>
-    );
+  return (
+    <MenuItem
+      active={selected === title}
+      onClick={() => setSelected(title)}
+      icon={icon}
+      className="hover:!bg-[unset]"
+    >
+      <Link href={to} className="hover:!bg-[unset]">
+        <Typography className="!text-[16px] !font-Poppins text-black dark:text-white">
+          {title}
+        </Typography>
+      </Link>
+    </MenuItem>
+  );
 };
 
 const AdminSidebar = () => {
-    const { user } = useSelector((state: any) => state.auth);
-    const [logout, setlogout] = useState(false);
-    const [isCollapsed, setIsCollapsed] = useState(false);
-    const [selected, setSelected] = useState("Dashboard");
-    const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme();
+  const { user } = useSelector((state: any) => state.auth);
+  const [logout, setlogout] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-    useEffect(() => setMounted(true), []);
+  useEffect(() => setMounted(true), []);
 
-    if (!mounted) {
-      return null;
-    }
+  if (!mounted) {
+    return null;
+  }
 
-    const logoutHandler = async () => {
-        Cookies.remove("accessToken");
-        Cookies.remove("refreshToken");
-        window.location.reload();
-    };
+  const logoutHandler = async () => {
+    Cookies.remove("accessToken");
+    Cookies.remove("refreshToken");
+    window.location.reload();
+  };
 
-    return (
-        <Box
-        sx={{
-            "& .pro-sidebar-inner": {
-              background: `${
-                theme === "dark" ? "#111C43 !important" : "#fff !important"
-              }`,
-            },
-            "& .pro-icon-wrapper": {
-              backgroundColor: "transparent !important",
-            },
-            "& .pro-inner-item:hover": {
-              color: "#868dfb !important",
-            },
-            "& .pro-menu-item.active": {
-              color: "#6870fa !important",
-            },
-            "& .pro-inner-item": {
-              padding: "5px 35px 5px 20px !important",
-              opacity: 1,
-            },
-            "& .pro-menu-item": {
-              color: `${theme !== "dark" && "#000"}`,
-            },
-          }}
-          className="!bg-white dark:bg-[#111C43]"
-        >
-          <Sidebar
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${
+            theme === "dark" ? "#111C43 !important" : "#fff !important"
+          }`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+          opacity: 1,
+        },
+        "& .pro-menu-item": {
+          color: `${theme !== "dark" && "#000"}`,
+        },
+      }}
+      className="!bg-white dark:bg-[#111C43]"
+    >
+      <Sidebar
         collapsed={isCollapsed}
         style={{
           position: "fixed",
@@ -110,9 +110,217 @@ const AdminSidebar = () => {
           width: isCollapsed ? "0%" : "16%",
         }}
       >
+        <Menu>
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <ArrowForwardIosIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Link href="/" className="block">
+                  <h3 className="text-[25px] font-Poppins uppercase dark:text-white text-black">
+                    LS
+                  </h3>
+                </Link>
+                <IconButton
+                  onClick={() => setIsCollapsed(!isCollapsed)}
+                  className="inline-block"
+                >
+                  <ArrowBackIosIcon className="text-black dark:text-[#ffffffc1]" />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
 
-         </Sidebar>
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <Image
+                  alt="profile-user"
+                  width={100}
+                  height={100}
+                  src={user.avatar ? user.avatar.url : avatarDefault}
+                  style={{
+                    cursor: "pointer",
+                    borderRadius: "50%",
+                    border: "3px solid #5b6fe6",
+                  }}
+                />
+              </Box>
+              <Box textAlign="center">
+                <Typography
+                  variant="h4"
+                  className="!text-[20px] text-black dark:text-[#ffffffc1]"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  {user?.name}
+                </Typography>
+                <Typography
+                  variant="h6"
+                  sx={{ m: "10px 0 0 0" }}
+                  className="!text-[20px] text-black dark:text-[#ffffffc1] capitalize"
+                >
+                  - {user?.role}
+                </Typography>
+              </Box>
+            </Box>
+          )}
 
-        </Box>
-    )
-}
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Dashboard"
+              to="/admin"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h5"
+              sx={{ m: "15px 0 5px 25px" }}
+              className="!text-[18px] text-black dark:text-[#ffffffc1] capitalize !font-[400]"
+            >
+              {!isCollapsed && "Dữ liệu"}
+            </Typography>
+            <Item
+              title="Người dùng"
+              to="/admin/users"
+              icon={<GroupsIcon className="text-black dark:text-white" />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Item
+              title="Hóa đơn"
+              to="/admin/invoices"
+              icon={
+                <ReceiptOutlinedIcon className="text-black dark:text-white" />
+              }
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h5"
+              className="!text-[18px] text-black dark:text-[#ffffffc1] capitalize !font-[400]"
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              {!isCollapsed && "Nội dung"}
+            </Typography>
+            <Item
+              title="Tạo khóa học"
+              to="/admin/create-course"
+              icon={<VideoCallIcon className="text-black dark:text-white" />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Khóa học"
+              to="/admin/courses"
+              icon={
+                <OndemandVideoIcon className="text-black dark:text-white" />
+              }
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h5"
+              className="!text-[18px] text-black dark:text-[#ffffffc1] capitalize !font-[400]"
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              {!isCollapsed && "Tùy chỉnh"}
+            </Typography>
+           
+            <Item
+              title="Danh mục"
+              to="/admin/categories"
+              icon={<WysiwygIcon className="text-black dark:text-white" />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h5"
+              className="!text-[18px] text-black dark:text-[#ffffffc1] capitalize !font-[400]"
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              {!isCollapsed && "Quản lý"}
+            </Typography>
+            <Item
+              title="Thành viên"
+              to="/admin/team"
+              icon={
+                <PeopleOutlinedIcon className="text-black dark:text-white" />
+              }
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              className="!text-[18px] text-black dark:text-[#ffffffc1] capitalize !font-[400]"
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              {!isCollapsed && "Phân tích"}
+            </Typography>
+            <Item
+              title="Khóa học"
+              to="/admin/courses-analytics"
+              icon={
+                <BarChartOutlinedIcon className="text-black dark:text-white" />
+              }
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Đơn hàng"
+              to="/admin/orders-analytics"
+              icon={<MapOutlinedIcon className="text-black dark:text-white" />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Item
+              title="Người dùng"
+              to="/admin/users-analytics"
+              icon={
+                <ManageHistoryIcon className="text-black dark:text-white" />
+              }
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              className="!text-[18px] text-black dark:text-[#ffffffc1] capitalize !font-[400]"
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              {!isCollapsed && "Tùy Chọn"}
+            </Typography>
+            <div onClick={logoutHandler}>
+              <Item
+                title="Đăng xuất"
+                to="/"
+                icon={<ExitToAppIcon className="text-black dark:text-white" />}
+                selected={selected}
+                setSelected={setSelected}
+              />
+            </div>
+          </Box>
+        </Menu>
+      </Sidebar>
+    </Box>
+  );
+};
+
+export default AdminSidebar;
