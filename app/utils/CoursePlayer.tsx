e76f82c53dced4fef7dbe6d90011014ce91f0c90
nghiakma <lam47788@gmail.com>
2024-10-10 18:27:18 +0700

    fix Preview

--- app/utils/CoursePlayer.tsx
+++ app/utils/CoursePlayer.tsx
@@ -1,43 +1,38 @@
 import React, { FC, useEffect, useState } from "react";
 import axios from "axios";
-
+import MuxPlayer from "@mux/mux-player-react"; 
 type Props = {
   videoUrl: string;
   title: string;
 };
 
 const CoursePlayer: FC<Props> = ({ videoUrl }) => {
-  const [videoData, setVideoData] = useState({
-    otp: "",
-    playbackInfo: "",
-  });
+  const [playbackId, setPlaybackId] = useState<string>("");
 
   useEffect(() => {
     axios
-      .post("http://localhost:8000/api/v1/getVdoCipherOTP", {
-        videoId: videoUrl,
+      .get("http://localhost:8000/api/v1/getMuxVideoOTP", {
+        params: {
+          videoId: videoUrl
+        }
       })
       .then((res) => {
-        setVideoData(res.data);
+        const playbackId = res.data.data.playback_ids[0]?.id;
+        setPlaybackId(playbackId);
       });
   }, [videoUrl]);
 
   return (
     <div style={{position:"relative",paddingTop:"56.25%",overflow:"hidden"}}>
-      {videoData.otp && videoData.playbackInfo !== "" && (
-        <iframe
-          src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=yhSoI6rb4DYEOhvk`}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            border: 0
-          }}
-          allowFullScreen={true}
-          allow="encrypted-media"
-        ></iframe>
+      {playbackId  !== "" && (
+        <MuxPlayer
+        playbackId={playbackId}
+        metadata={{
+          video_id: "video-id-54321",
+          video_title: "Test video title",
+          viewer_user_id: "user-id-007",
+        }}
+      />
       )}
     </div>
   );
